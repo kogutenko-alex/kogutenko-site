66448886051f8ed2e159e23f9b652166b9a4f97f
Aleksandr Kogutenko <oleksandr.kogutenko@gmail.com>
2021-11-05 03:16:32 +0200

    add slider

--- js/main-script.js
+++ js/main-script.js
@@ -118,3 +118,117 @@ if (isMobile.any()) {
 } else {
    body.classList.add('mouse');
 }
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 2000);
+
+function nextSlide() {
+   goToSlide(currentSlide++);
+}
+function previousSlide() {
+   goToSlide(currentSlide--);
+}
+//9
+function goToSlide(n) {
+   var allSlides = 0;
+   var allSlides_ = 0;
+   if (window.screen.width > 1280) {
+      console.log("------------start--------------");
+      allSlides_ = slides.length / 4; // 2.25
+      allSlides = Math.ceil(allSlides_); //3
+      console.log("all slides_: " + allSlides_);
+      console.log("all slides : " + allSlides);
+      if (n > allSlides) {
+         n = 0;
+         currentSlide = 0;
+      }
+      console.log("n : " + n);
+      var k = 0, index = 0, i = 0;
+      console.log(slides);
+      while (i < slides.length) {
+         if (slides[i].classList.contains('showing')) {
+            console.log("remove " + i)
+            slides[i].classList.remove('showing');
+            index = i;
+         }
+         i++;
+      }
+      console.log("--------------------------");
+      console.log(slides);
+      if (index == --slides.length) {
+         index = -1;
+      }
+      while (index < slides.length && k < 4) {
+         slides[++index].classList.add('showing');
+         k++;
+      }
+      console.log("-----------ending------------");
+
+   }
+   if (window.screen.width <= 1280) {
+      console.log("------------start--------------");
+      allSlides_ = slides.length / 2; // 2.25
+      allSlides = Math.ceil(allSlides_); //3
+      console.log("all slides_: " + allSlides_);
+      console.log("all slides : " + allSlides);
+      if (n > allSlides) {
+         n = 0;
+         currentSlide = 0;
+      }
+      console.log("n : " + n);
+      var k = 0, index = 0, i = 0;
+      console.log(slides);
+      while (i < slides.length) {
+         if (slides[i].classList.contains('showing')) {
+            console.log("remove " + i)
+            slides[i].classList.remove('showing');
+            index = i;
+         }
+         i++;
+      }
+      console.log("--------------------------");
+      console.log(slides);
+      if (index == --slides.length) {
+         index = -1;
+      }
+      while (index < slides.length && k < 2) {
+         slides[++index].classList.add('showing');
+         k++;
+      }
+      console.log("-----------ending------------");
+
+   }
+
+}
+
+var playing = true;
+var pauseButton = document.getElementById('pause');
+function pauseSlideshow() {
+   pauseButton.innerHTML = 'Play';
+   playing = false;
+   clearInterval(slideInterval);
+}
+function playSlideshow() {
+   pauseButton.innerHTML = 'Pause';
+   playing = true;
+   slideInterval = setInterval(nextSlide, 2000);
+}
+pauseButton.onclick = function () {
+   if (playing) {
+      pauseSlideshow();
+   } else {
+      playSlideshow();
+   }
+};
+
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+next.onclick = function () {
+   pauseSlideshow();
+   nextSlide();
+};
+previous.onclick = function () {
+   pauseSlideshow();
+   previousSlide();
+};
